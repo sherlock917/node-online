5f7b0d2d644f82d0d2f7cc54d1a0b2afff4a12fa
SherlockZhong <sherlock.z0917@gmail.com>
2015-03-17 09:32:16 +0800

    fixed functions

--- views/js/index.js
+++ views/js/index.js
@@ -1,74 +1,82 @@
 (function () {
 
+  // functino to load
+  initSocket();
+  initDom();
 
-  var socket = io(location.host);
-  var pid;
-
-  socket.on('processing', function (data) {
-    hint(data, 0x00f);
-  });
-  socket.on('success', function (data) {
-    hint(data, 0x0f0);
-  });
-  socket.on('error', function (data) {
-    hint(data, 0xf00);
-  });
-  socket.on('stdout', function (data) {
-    decode(data);
-  });
-  socket.on('stderr', function (data) {
-    decode(data, true);
-  });
-  socket.on('pid', function (data) {
-    pid = data;
-  });
-  socket.on('close', function (data) {
-    $('#log').innerHTML += '<br><p>** exit with code ' + data + ' **</p><br>'; 
-    $('#log').scrollTop = 999999999;
-    hint('done running code', 0x0f0);
-    pid = null;
-  });
-  socket.on('online', function (data) {
-    $('#online').innerHTML = data;
-  });
-  socket.on('running', function (data) {
-    $('#running').innerHTML = data;
-  });
-
-  $$.bind($('#run'), 'click', function (e) {
-    if ($('#input').value != '') {
-      $('#log').innerHTML = '';
-      hint('code submitted', 0x0f0);
-      socket.emit('run', $('#input').value);
-    } else {
-      hint('no code to submit', 0xf00);
-    }
-  });
-
-  $$.bind($('#halt'), 'click', function (e) {
-    if (pid) {
-      socket.emit('halt', pid);
-    } else {
-      hint('no code running', 0xf00);
-    }
-  });
-
-  var tab = 0;
-  var shift = false;
-
-  $$.bind($('#input'), 'keyup', function (e) {
-    if ($('#input').value == '') {
-      tab = 0;
-    }
-    if (e.keyCode == 13) {
-      appendTab();
-    } else if (e.keyCode == 16) {
-      shift = true;
-    } else if (e.keyCode == 221 && shift && tab > 0) {
+  // global variables
+  var socket,
+      pid,
+      tab = 0,
       shift = false;
-      removeTab();
-    }
-  });
+
+  // inits socket, binds event to handlers
+  function initSocket() {
+    socket = io(location.host);
+    socket.on('processing', function (data) {
+      hint(data, 0x00f);
+    });
+    socket.on('success', function (data) {
+      hint(data, 0x0f0);
+    });
+    socket.on('error', function (data) {
+      hint(data, 0xf00);
+    });
+    socket.on('stdout', function (data) {
+      decode(data);
+    });
+    socket.on('stderr', function (data) {
+      decode(data, true);
+    });
+    socket.on('pid', function (data) {
+      pid = data;
+    });
+    socket.on('close', function (data) {
+      $('#log').innerHTML += '<br><p>** exit with code ' + data + ' **</p><br>'; 
+      $('#log').scrollTop = 999999999;
+      hint('done running code', 0x0f0);
+      pid = null;
+    });
+    socket.on('online', function (data) {
+      $('#online').innerHTML = data;
+    });
+    socket.on('running', function (data) {
+      $('#running').innerHTML = data;
+    });
+  }
+  
+  // bind dom events
+  function initDom () {
+    $$.bind($('#run'), 'click', function (e) {
+      if ($('#input').value != '') {
+        $('#log').innerHTML = '';
+        hint('code submitted', 0x0f0);
+        socket.emit('run', $('#input').value);
+      } else {
+        hint('no code to submit', 0xf00);
+      }
+    });
+    $$.bind($('#halt'), 'click', function (e) {
+      if (pid) {
+        socket.emit('halt', pid);
+      } else {
+        hint('no code running', 0xf00);
+      }
+    });
+    $$.bind($('#input'), 'keyup', function (e) {
+      if ($('#input').value == '') {
+        tab = 0;
+      }
+      if (e.keyCode == 13) {
+        appendTab();
+      } else if (e.keyCode == 16) {
+        shift = true;
+      } else if (e.keyCode == 221 && shift && tab > 0) {
+        shift = false;
+        removeTab();
+      }
+    });
+  }
 
   function appendTab () {
     var latest = $('#input').value.substr(-5).split('');
