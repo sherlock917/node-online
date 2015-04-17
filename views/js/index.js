9816a171acfeafb959d43a8bef613c78a8cea440
SherlockZhong <sherlock.z0917@gmail.com>
2015-04-17 15:15:34 +0800

    fixed

--- views/js/index.js
+++ views/js/index.js
@@ -116,7 +116,8 @@
       'break', 'do', 'instanceof', 'typeof', 'case', 'else', 'new', 'var',
       'catch', 'finally', 'return', 'void', 'continue', 'for', 'switch',
       'while', 'debugger', 'function', 'this', 'with', 'default','if',
-      'throw', 'delete', 'in', 'try', 'require'
+      'throw', 'delete', 'in', 'try', 'require', 'console',
+      'setTimeout', 'setInterval'
     ];
     for (var i = 0; i < keys.length; i++) {
       TrieTree.grow(keys[i]);
